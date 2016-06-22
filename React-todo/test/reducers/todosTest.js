6bcfc02a92daf46bf51a89fdbc9c2c71c11fa3e9
guo liu <guo liu>
2016-06-22 23:36:25 +0800

    add:todos.TOOGGLE_TODO test

--- React-todo/test/reducers/todosTest.js
+++ React-todo/test/reducers/todosTest.js
@@ -11,6 +11,16 @@ describe('todos reducers',() => {
       .be
       .deep
       .eql([{text: 'Return the test',completed:false,id:0}])
-  })
+  });
+
+  it('should handle TOOGGLE_TODO',()=>{
+    expect(todos(
+      [{text: 'Return the test',completed:false,id:0}],
+      {type:'TOGGLE_TODO',id:0}))
+    .to
+    .be
+    .deep
+    .eql([{text: 'Return the test',completed:true,id:0}])
+  });
 
 })
